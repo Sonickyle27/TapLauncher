090c44e26a8c740b8802005b2cb9a09f41cadf62
Kyle Applin <kylesnetwork@gmail.com>
2015-06-23 13:52:41 +0100

    Adding file

---
+++ StartServer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mytitle="Spigot Server Manager"
+echo -e '\033]2;'$mytitle'\007'
+
+while true
+do
+clear
+echo "[INFO]: Starting server..."
+echo
+#Type the command that you want to be executed here.
+java -Xmx1024M -Xms1024M -jar Spigot.jar
+echo
+echo "[INFO]: Server was either stopped manually, or crashed."
+echo "[INFO]: Server is restarting in 30 seconds. Press Ctrl+C to cancel."
+#Time is in seconds. You can change it accordingly.
+sleep 30
+done
