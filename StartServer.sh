1f138d2a210aef2ad89a1b2c583ee6af7731ec7d
Kyle Applin <kylesnetwork@gmail.com>
2015-09-04 20:42:38 +0100

    Added whiptail dialog boxes.
    The script now contains whiptail dialogue boxes that ask if you really want to start the server. It also asks if you want to restart the server after shutting the server down safely. The script still handles crashes and still auto-restarts if the server does crash.

--- StartServer.sh
+++ StartServer.sh
@@ -3,7 +3,9 @@
 mytitle="Minecraft Server Manager"
 echo -e '\033]2;'$mytitle'\007'
 
-while true
+whiptail --yesno "Do you want to start the server? To safely stop the server, you should type 'stop' at the server command prompt and then press Ctrl+C when told to." 10 50
+if [ $? -eq 0 ]
+then while true
 do #It loops forever! Basically, if the server exits or crashes, it'll autorestart unless you Ctrl+C.
 clear
 echo "[INFO]: Starting server..."
@@ -12,9 +14,22 @@ echo
 #The defaunt command launches "Spigot.jar" and gives it 1GB of memory.
 #Change the Spigot.jar accordingly if it has a different name or isn't located in the same folder as this script.
 java -Xmx1024M -Xms1024M -jar Spigot.jar
-echo
-echo "[INFO]: Server was either stopped manually, or crashed."
-echo "[INFO]: Server is restarting in 30 seconds. Press Ctrl+C to cancel."
+#The lines below handle restarts and crashes.
+if [ $? -eq 0 ]
+then echo "[INFO]: Server was succesfully stopped."
+whiptail --yesno "Do you want to restart the server (yes) or quit (no)?" 10 50  
+else
+echo "[WARN]: It seems that the server crashed."
+fi
+if [ $? -eq 0 ]
+then
+echo "[INFO]: Server is restarting in 10 seconds. Press Ctrl+C to cancel."
 #Time is in seconds. You can change it accordingly.
-sleep 30
+sleep 10
+else
+echo "[INFO]: Quitting."
+sleep 1
+exit
+fi
 done
+fi
